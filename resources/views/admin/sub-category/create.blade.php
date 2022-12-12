eb2e1e13b405859b7a5987ad3ae663ebecc5c925
Sunday-Binakin <Sundaybinakin@gmail.com>
2022-12-12 01:36:12 +0000

    creating content

--- resources/views/admin/sub-category/create.blade.php
+++ resources/views/admin/sub-category/create.blade.php
@@ -1,5 +1,36 @@
 @extends('admin.layouts.master')
 @section('page-content')
-<h2>create sub cate</h2>
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h4>Create Sub Category</h4>
+            </div>
+            <form action="" method="POST">
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="subcategory_name"><h6><strong>Enter Sub Category Name</strong></h6></label>
+                        <input type="text" class="form-control" id="subcategory_name" name="subcategory_name" placeholder="Electronics">
+                    </div>
+                    <div class="form-group">
+                        <label for="category_name">Select Category</label>
+                        <select class="form-control"  name="category_id">
+                            <option value="">Select Category</option>
+                            <option value="1">Electronics</option>
+                            <option value="2">Fashion</option>
+                            <option value="3">Sports</option>
+                            <option value="4">Home & Kitchen</option>
+                            <option value="5">Grocery</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button class="btn btn-primary">Create sub Category</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
 
